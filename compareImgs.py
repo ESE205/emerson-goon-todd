f105a3b7179d8122b4d9d66871ba7d74273efecc
TomGoon <tgoon@wustl.edu>
2019-02-24 22:58:50 -0600

    more imgs

--- compareImgs.py
+++ compareImgs.py
@@ -6,13 +6,13 @@ import cv2 as cv
 camera = PiCamera()
 camera.resolution = (1024,1024)
 camera.iso = 200
-camera.shutter_speed = camera.exposure_speed
-sleep(2)
-camera.exposure_mode = 'off'
-gain = camera.awb_gains
-sleep(2)
-camera.awb_mode = 'off'
-camera.awb_gains = gain
+#camera.shutter_speed = camera.exposure_speed
+#sleep(2)
+#camera.exposure_mode = 'off'
+#gain = camera.awb_gains
+#sleep(2)
+#camera.awb_mode = 'off'
+#camera.awb_gains = gain
 
 cap1 = camera.capture('images/img1.jpg')
 sleep(5)
