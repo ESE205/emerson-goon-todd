5a807ff1d7d5812565672dfb2cf1e2efdaf44810
TomGoon <tgoon@wustl.edu>
2019-02-24 22:05:03 -0600

    Compare Images

---
+++ compareImgs.py
@@ -0,0 +1,26 @@
+from picamera import PiCamera
+from time import sleep
+import numpy as np
+import cv2 as cv
+
+camera = PiCamera()
+camera.resolution = (1024,1024)
+
+cap1 = camera.capture('images/img1.jpg')
+sleep(5)
+cap2 = camera.capture('images/img2.jpg')
+sleep(1)
+
+img1 = cv.imread('images/img1.jpg')
+img2 = cv.imread('images/img2.jpg')
+
+gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+
+diff = cv.absdiff(gray1, gray2)
+thresh = 20
+diff[diff < thresh] = 0
+diff[diff >= thresh] = 200
+numDiffPix = np.sum(diff)/200
+
+print(numDiffPix)
