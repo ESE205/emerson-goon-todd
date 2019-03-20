34f557fa1063fc4b9d02a11090efe6847997d84e
Thomas Emerson <ThomasEmerson@thomassmbp1423.dhcp.wustl.edu>
2019-03-19 19:53:08 -0500

    COMMIT_MESSAGE

---
+++ pi.py
@@ -0,0 +1,40 @@
+# bottle is the module which will manage our API routes/requests
+from bottle import route, run, static_file
+
+# requests handles external API calls
+import requests
+
+# mysql connector allows us to connect to a mysql database
+import mysql.connector
+
+# allows us to utilize our config file
+import json
+
+# load our config json file into a python dictionary
+config = json.loads(open('config.json').read())
+
+# establish mysql connection
+db = mysql.connector.connect(
+    host=config['mysqlHost'],
+    user=config['mysqlUser'],
+    passwd=config['mysqlPassword'],
+    database=config['mysqlDatabase']
+)
+
+print('Connection established')
+
+# the cursor is used to query the database
+cursor = db.cursor() 
+
+qString = 'INSERT INTO wifiMAC (macAdd, timeStampe, vendor) VALUES (%s, %s, %s)'
+values = ('we', 'made', 'it')
+ 
+cursor.execute(qString, values)
+db.commit()
+
+cursor.execute("SELECT * FROM wifiMAC;")
+results = cursor.fetchall()
+for r in results:
+    print(r)
+cursor.close()
+db.close()
---
+++ tester.php
@@ -0,0 +1,37 @@
+
+
+<html>
+<head>
+<title>Process Record</title>
+</head>
+<body>
+ <h1>Pet Record added.</h1>
+
+
+<?php
+  $con = mysqli_connect("busybear.c2qo2vmqahc3.us-east-2.rds.amazonaws.com","root","I<3madison","BusyBear");
+ // Check connection
+  if (mysqli_connect_errno())
+  {
+     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+$sql="INSERT INTO wifiMAC (macAdd,timeStampe,vendor) 
+	   VALUES ('comand','comand','line')";
+	   
+
+
+	$result = mysqli_query($con, $sql);
+	if ( false===$result ) {
+	  printf("error: %s\n", mysqli_error($con));
+	}
+	else {
+  echo "success";
+  }
+  mysqli_close($con);
+  ?>
+  
+  
+</body>
+</html>
