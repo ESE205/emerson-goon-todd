e41dd5301638bb60d071b92ed5e97a10895f5a3a
temerson1 <46689093+temerson1@users.noreply.github.com>
2019-03-21 21:28:02 -0500

    Add files via upload

---
+++ AddTest.php
@@ -0,0 +1,224 @@
+
+<html>
+<title>Oracle Project</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+.fa-anchor,.fa-coffee {font-size:200px}
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    width: 100%;
+    background-color: #F2453E;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+div {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+#test {
+  width: 20%;
+    padding: 16px 20px;
+    border: none;
+    border-radius: 4px;
+    background-color: #f1f1f1;
+
+}
+textarea {
+    width: 100%;
+    height: 150px;
+    padding: 12px 20px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    background-color: #f8f8f8;
+    font-size: 16px;
+    resize: none;
+}
+.container {
+    position: relative;
+    padding-left: 20px;
+    padding-right: 20px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    font-size: 16px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+/* Hide the browser's default radio button */
+.container input {
+    position: absolute;
+    opacity: 0;
+}
+
+/* Create a custom radio button */
+.checkmark {
+    position: absolute;
+    top: 2;
+    left: 0;
+    height: 16px;
+    width: 16px;
+    background-color: #ccc;
+    border-radius: 50%;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+    background-color: black;
+}
+
+/* When the radio button is checked, add a blue background */
+.container input:checked ~ .checkmark {
+    background-color: #2196F3;
+}
+
+/* Create the indicator (the dot/circle - hidden when not checked) */
+.checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+}
+
+/* Show the indicator (dot/circle) when checked */
+.container input:checked ~ .checkmark:after {
+    display: block;
+}
+
+/* Style the indicator (dot/circle) */
+.container .checkmark:after {
+  top: 5.6px;
+  left: 5.6px;
+  width: 5.12px;
+  height: 5.12px;
+  border-radius: 50%;
+  background: white;
+}
+</style>
+<body>
+
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="http://3.94.46.108/BusyBear_Launch.php" class="w3-bar-item w3-button w3-padding-large w3-hover-white">Home</a>
+    <a href="http://3.94.46.108/BusyBear_Olin.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Olin</a>
+    <a href="http://3.94.46.108/Add_Animal.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Mallinckrodt</a>
+    <a href="http://3.94.46.108/AddTest.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-white">Test Add</a>
+    
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+    <a href="http://localhost/Add_Pet.php" class="w3-bar-item w3-button w3-padding-large">Add a Pet</a>
+    <a href="http://localhost/Add_Animal.php" class="w3-bar-item w3-button w3-padding-large">Add an Animal</a>
+    <a href="http://localhost/ViewDatabase.php" class="w3-bar-item w3-button w3-padding-large">View Pets</a>
+    <a href="http://localhost/View_Table_Animals.php" class="w3-bar-item w3-button w3-padding-large">View Animals</a>
+  </div>
+</div>
+
+<!-- Header -->
+
+<!-- First Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+      <h1 style="text-align:center" ><br>Add Test Entry</h1>
+      <h5 class="w3-padding-small"></h5>
+      <div>
+    <form method="post" action="UpdateTest.php">
+    
+      <label for="macAdd">MAC Address:</label>
+      <input type="text" id="macAdd" name="macAdd" placeholder="MAC Address...">
+
+      <label for="timeStampe">Time Stamp:</label>
+      <input type="text" id="timeStampe" name="timeStampe" placeholder="Time stamp...">
+
+      <label for="vendor">Vendor:</label>
+      <input type="text" id="vendor" name="vendor" placeholder="Vendor of MAC Address..">
+
+      <label for="pictureE">Select a Picture file:</label>
+      <input type="file" id="pictureE" name="pictureE" > <br><br>
+
+    
+  
+  
+  <input type="submit" value="Submit"/>
+  
+  </form>
+</div>
+</div>
+  </div>
+</div> 
+
+
+<!-- Second Grid -->
+
+<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+    <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity">  
+  <div class="w3-xlarge w3-padding-32">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+ </div>
+ <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">Emerson Industrial</a></p>
+</footer>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
