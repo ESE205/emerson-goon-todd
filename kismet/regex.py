f6e97ee8c3ab0bf664f038780bff5295f651cd25
TomGoon <tgoon@wustl.edu>
2019-03-22 21:56:57 -0500

    Updated code to upload to database

--- kismet/regex.py
+++ kismet/regex.py
@@ -1,14 +1,39 @@
+
 import re
 import datetime
 import pprint
 import requests
 import json
 
+# bottle is the module which will manage our API routes/requests
+from bottle import route, run, static_file
+
+# mysql connector allows us to connect to a mysql database
+import mysql.connector
+
+# load our config json file into a python dictionary
+config = json.loads(open('config.json').read())
+
+# establish mysql connection
+db = mysql.connector.connect(
+    host=config['mysqlHost'],
+    user=config['mysqlUser'],
+    passwd=config['mysqlPassword'],
+    database=config['mysqlDatabase']
+)
+
+# the cursor is used to query the database
+cursor = db.cursor() 
+
+qString = 'INSERT INTO wifiMAC (macAdd, vendor) VALUES (%s, %s)'
+
+print('Connection established')
+
 # Regular Expression, only gets MAC Addresses after it sees "device"
 MAC_regex = re.compile(r"(?<=\bdevice\b\s)\w\w[:]\w\w[:]\w\w[:]\w\w[:]\w\w[:]\w\w")
 
 # Get the file in read mode
-testFile = open("elliot_min.txt","r")
+testFile = open("test3.txt","r")
 
 # Counter for total number of MAC Addresses seen including repeats
 count = 0
@@ -29,11 +54,28 @@ MAC_URL = 'http://macvendors.co/api/%s'
 
 # Loop through the addresses in the Dictionary and print the address with timestamp and vendor information 
 for adrs in MACdict: 
-	print(adrs + ' ' + MACdict[adrs])
-	req = requests.get(MAC_URL % adrs)
-	pprint.pprint(req.json())
+  print(adrs + ' ' + MACdict[adrs])
+  req = requests.get(MAC_URL % adrs)
+  obj = req.json()
+  for key, value in obj.items():
+    if('company' in value):
+      values = (adrs,value['company'])
+      print(value['company'])
+    else:
+      values = (adrs,'Null')
+      print('Null')
+    cursor.execute(qString,values)
+
+db.commit()
+
+cursor.execute("SELECT * FROM wifiMAC;")
+results = cursor.fetchall()
+for r in results:
+    print(r)
+cursor.close()
+db.close()
 
 # Print total number of devices and number of MAC Addresses.
 # In theory, the same device may show up twice given a long enough period, unsure
-print('Total Devices: '+ str(len(MACdict)))
+print('Total Devices: ' + str(len(MACdict)))
 print('Total # of MAC Addresses: ' + str(count))
