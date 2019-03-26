f1826ca71fec6ef9c9eb4a7f5409fe538e96e737
temerson1 <46689093+temerson1@users.noreply.github.com>
2019-03-25 23:28:50 -0500

    Update BusyBear_BD.php

--- BusyBear_BD.php
+++ BusyBear_BD.php
@@ -283,14 +283,14 @@ function drawChart() {
 <!-- Footer -->
 <footer class="w3-container w3-padding-64 w3-center w3-opacity">  
   <div class="w3-xlarge w3-padding-32">
-    <i class="fa fa-facebook-official w3-hover-opacity" <a href="www.linkedin.com/in/ThomasGEmerson"</a>></i>
-    <i class="fa fa-instagram w3-hover-opacity" <a href="www.linkedin.com/in/ThomasGEmerson"</a>></i>
-    <i class="fa fa-snapchat w3-hover-opacity" <a href="www.linkedin.com/in/ThomasGEmerson"</a>></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity" <a href="www.linkedin.com/in/ThomasGEmerson"</a>></i>
-    <i class="fa fa-twitter w3-hover-opacity" <a href="www.linkedin.com/in/ThomasGEmerson"</a>></i>
-    <i class="fa fa-linkedin w3-hover-opacity" <a href="www.linkedin.com/in/ThomasGEmerson"</a>></i>
+    <i class="fa fa-facebook-official w3-hover-opacity" <a href="https://www.linkedin.com/in/ThomasGEmerson"</a></i>
+    <i class="fa fa-instagram w3-hover-opacity" <a href="https://www.linkedin.com/in/ThomasGEmerson"</a></i>
+    <i class="fa fa-snapchat w3-hover-opacity" <a href="https://www.linkedin.com/in/ThomasGEmerson"</a></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity" <a href="https://www.linkedin.com/in/ThomasGEmerson"</a></i>
+    <i class="fa fa-twitter w3-hover-opacity" <a href="https://www.linkedin.com/in/ThomasGEmerson"</a></i>
+    <i class="fa fa-linkedin w3-hover-opacity" <a href="https://www.linkedin.com/in/ThomasGEmerson"</a></i>
  </div>
- <p>Powered by <a href="www.linkedin.com/in/ThomasGEmerson" target="_blank">Emerson Industrial</a></p>
+ <p>Powered by <a href="https://www.linkedin.com/in/ThomasGEmerson" target="_blank">Emerson Industrial</a></p>
 </footer>
 
 <script>
