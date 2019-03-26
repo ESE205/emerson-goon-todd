525379ed301724b1066c3a81af1ac895eed8c315
temerson1 <46689093+temerson1@users.noreply.github.com>
2019-03-25 23:21:32 -0500

    Update BusyBear_BD.php

--- BusyBear_BD.php
+++ BusyBear_BD.php
@@ -268,6 +268,45 @@ function drawChart() {
 <br>
 <br>
 <br>
+    <!-- Second Grid -->
+
+<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+    <h1 class="w3-margin w3-xlarge">Quote of the day: Wonder which Canada Goose jacket I'll wear today
+    </h1>
+    <p class="w3-text-grey">
+       <?php
+    echo "Today is " . date("m-d-Y");
+    echo " and the day is " . date("l");?>
+    </p>
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity">  
+  <div class="w3-xlarge w3-padding-32">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+ </div>
+ <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">Emerson Industrial</a></p>
+</footer>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+</script>
+
+</body>
+</html>
 <br>
 <br>
 <br>
