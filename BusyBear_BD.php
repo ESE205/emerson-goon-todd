992cee4e812151152037e1f49d6929b8e98dc160
temerson1 <46689093+temerson1@users.noreply.github.com>
2019-04-03 18:43:28 -0500

    Update BusyBear_BD.php

--- BusyBear_BD.php
+++ BusyBear_BD.php
@@ -135,7 +135,7 @@ textarea {
 <div class="w3-row-padding w3-padding-64 w3-container">
   <div class="w3-content">
     <div class="w3-twothird">
-      <h1 style="text-align:center" ><br>How Busy is Bear Den?</h1>
+      <h1 style="text-align:center" ><br>How Busy is Bear's Den?</h1>
       <h5 class="w3-padding-small"></h5>
       <div>
     <div id="piechart"></div>
